3add24a6f75ebf850a8a518ce397ee5301d6da1c
Leopold David <leopolddavid@MacBook-Pro-de-tkt.local>
2026-09-10 15:56:21 +0200

    1.1.0

--- public/config.js
+++ public/config.js
@@ -1,7 +1,7 @@
 window.WOMM_CONFIG = {
   environment: "PRODUCTION",
-  version: "1.0.0",
+  version: "1.1.0",
   developerConfidence: "73%",
   statusText: "Probably fine.",
-  supportContact: "yes"
+  supportContact: "Sleeping"
 };
