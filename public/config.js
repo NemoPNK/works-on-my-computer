fdec44c3baf04d758413006baee4a49f1e41b4e5
Leopold David <leopolddavid@MacBook-Pro-de-tkt.local>
2026-09-10 15:37:38 +0200

    fix bug

--- public/config.js
+++ public/config.js
@@ -2,6 +2,6 @@ window.WOMM_CONFIG = {
   environment: "PRODUCTION",
   version: "1.0.0",
   developerConfidence: "73%",
-  statusText: "Probably fine."
-  support contact: "yes"
+  statusText: "Probably fine.",
+  supportContact: "yes"
 };
